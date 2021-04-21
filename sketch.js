b3e11b711edefad21567a3a8b11f509e713bda5f
vishnu418 <76873448+vishnu418@users.noreply.github.com>
2021-04-21 16:00:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+  var balloonPosition = database.ref('balloon/position')
+  
+    
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    changePosition(-1,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    changePosition(1,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    changePosition(0,-1);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    changePosition(0,+1);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function changePosition(x,y){
+  balloon.x = balloon.x + x+x+x+x+x+x+x+x;
+  balloon.y = balloon.y + y+y+y+y+y+y+y+y;
+}
+function readPosition(data){
+  position=data.val()
+  balloon.x=position.x
+  balloon.y=position.y
+  }
+  function showError(){
+    console.log("error")
+}
